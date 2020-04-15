3270eea57607d962f2c864702650bf34b1bc011b
Lukas Forst <lukas@forst.pw>
2020-04-15 15:30:58 +0200

    add proper production formatting

---
+++ src/main/kotlin/com/wire/ganymede/setup/logging/ProductionLayout.kt
@@ -0,0 +1,24 @@
+package com.wire.ganymede.setup.logging
+
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.CoreConstants
+import ch.qos.logback.core.LayoutBase
+
+/**
+ * Layout that is same as on the production servers in Wire.
+ */
+class ProductionLayout : LayoutBase<ILoggingEvent>() {
+
+    override fun doLayout(event: ILoggingEvent): String =
+        with(StringBuffer(128)) {
+            append("|")
+            append(String.format("level=%.1s", event.level.levelStr))
+            append("|")
+            append(event.loggerName)
+            append("|")
+            append(event.formattedMessage)
+            append(CoreConstants.LINE_SEPARATOR)
+            toString()
+        }
+}
